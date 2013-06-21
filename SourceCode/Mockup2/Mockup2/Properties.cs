8c5a1d1f7337bf783d3c11a3931070c4b39e6bd9
Pakorn Indhatep <pakorncs@yahoo.com>
2013-06-22 01:15:49 +0200

    Properties class created

---
+++ SourceCode/Mockup2/Mockup2/Properties.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mockup2
+{
+    public class Properties
+    {
+        public class Locations
+        {
+            public int id { get; set; }
+            public String name { get; set; }
+            public String l_lat { get; set; }
+            public String l_long { get; set; }
+        }
+        public class Projects
+        {
+            public int id { get; set; }
+            public String name { get; set; }
+        }
+        public class ResourceTypes
+        {
+            public int id { get; set; }
+            public String name { get; set; }
+            public String ending { get; set; }
+            public String icon_url { get; set; }
+        }
+        public class Resources
+        {
+            public int id { get; set; }
+            public String type_id { get; set; }
+            public String task_id { get; set; }
+            public String user_id { get; set; }
+            public String name { get; set; }
+            public String url { get; set; }
+            public String size { get; set; }
+            public String date { get; set; }
+        }
+        public class Roles
+        {
+            public int id { get; set; }
+            public String name { get; set; }
+        }
+        public class Statuses
+        {
+            public int id { get; set; }
+            public String name { get; set; }
+            public String url { get; set; }
+        }
+        public class Task_Types
+        {
+            public int id { get; set; }
+            public String name { get; set; }
+        }
+        public class Tools
+        {
+            public int id { get; set; }
+            public String name { get; set; }
+            public String url { get; set; }
+        }
+
+    }
+}
