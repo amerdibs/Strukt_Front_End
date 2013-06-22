d514d1de8610df5f5e43164b610eb231f22cfa4b
amerdibs <amerdibs@gmail.com>
2013-06-22 16:09:46 +0200

    Workflows.cs parameters are defined

--- SourceCode/Mockup2/Mockup2/Workflows.cs
+++ SourceCode/Mockup2/Mockup2/Workflows.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Mockup2
 {
     class Workflows
     {
+
+    public int id { get; set; }
+    public int user_id { get; set; }
+    public int task_id { get; set; }
+    public string type { get; set; }
+    public DateTime created_at { get; set; }
+    public DateTime updated_at { get; set; }
+
     }
 }
