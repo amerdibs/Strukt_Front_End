cd8ea420727449395c5aec5d14727158a7bc4111
amerdibs <amerdibs@gmail.com>
2013-06-24 12:18:31 +0200

    Testing Properties_Strukt.Statuses.getStatusesByID(string strID) Testing Properties_Strukt.Locations.getLocationsAll()

--- SourceCode/Mockup2/Mockup2/TestFunction.cs
+++ SourceCode/Mockup2/Mockup2/TestFunction.cs
@@ -21,8 +21,13 @@ namespace Mockup2
 
         private void btnTest_Click(object sender, EventArgs e)
         {
-            List<Condition.Precondtion_set> precon = Condition.Precondtion_set.getPrecondtion_setsByTaskID("2031343073");
-            MessageBox.Show(precon[0].id);
+            List<Properties_Strukt.Status> status = Properties_Strukt.Status.getStatusByID("4");
+            MessageBox.Show(status[0].id);
+
+            //test for the getLocationall  List<Properties_Strukt.Location> location = Properties_Strukt.Location.getLocationAll();
+            
+           /* List<Condition.Precondtion_set> precon = Condition.Precondtion_set.getPrecondtion_setsByTaskID("2031343073");
+            MessageBox.Show(precon[0].id);*/
 
             //string json = JsonConvert.SerializeObject(precon, Formatting.Indented);
             //json = global.composeJSONforStrukt(json, Strukt.T_Precondition_set);
