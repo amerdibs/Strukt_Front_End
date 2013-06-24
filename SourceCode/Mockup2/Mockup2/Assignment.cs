1bd9ffa43103b6ce8124e6ad0b60446b779aa94e
TariiqKhaan <tariiq_khaan@hotmail.com>
2013-06-24 20:27:01 +0200

    Update Assignment.cs

--- SourceCode/Mockup2/Mockup2/Assignment.cs
+++ SourceCode/Mockup2/Mockup2/Assignment.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,5 +17,43 @@ namespace Mockup2
         public string message { get; set; }
         public DateTime created_at { set; get; }
         public DateTime updated_at { set; get; }
+
+        public static List<Assignment> getAssignmentByTargetUserID(string strTargetUserID)
+        {
+            try
+            {
+                string strAssign = global.getExtractValueFromRespond(global.getRespondFromStruktGet(Strukt.URL_Assignment, "?target_user_id=" + Strukt.Type_User + strTargetUserID));
+
+                if (strAssign == "")
+
+                    return null;
+
+                List<Assignment> strSnoopDog = JsonConvert.DeserializeObject<List<Assignment>>(strAssign);
+                return strSnoopDog;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+        public static List<Assignment> getAssignmentBySourceUserID(string strTargetUserID)
+        {
+            try
+            {
+                string strSource = global.getExtractValueFromRespond(global.getRespondFromStruktGet(Strukt.URL_Assignment, "?source_task_id=" + Strukt.Type_User + strTargetUserID));
+
+                if (strSource == "")
+
+                    return null;
+
+                List<Assignment> strAssignSource = JsonConvert.DeserializeObject<List<Assignment>>(strSource);
+                return strAssignSource;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
