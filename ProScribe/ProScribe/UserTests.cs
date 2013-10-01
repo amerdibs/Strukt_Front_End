88fd8caefa0b057d56a97d5cfd215687d2f17a0b
Pakorn Indhatep <pakorncs@yahoo.com>
2013-10-01 21:50:58 +0200

    Add test project

---
+++ ProScribe/ProScribe/UserTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Data;
+
+namespace Mockup2
+{
+    [TestFixture]
+    public class UserTests
+    {
+        [TestCase]
+        public void getUserNameByStruktIDTest(string strUserID)
+        {
+            DataRow dtRow = User.getUserNameByStruktID("");
+            Assert.AreEqual(null, dtRow);
+        }
+    }
+}
