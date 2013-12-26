2efa3dedfec06cbf67469eff5c100a78f88d7ec1
Pakorn Indhatep <pakorncs@yahoo.com>
2013-12-26 01:45:38 +0100

    Plug-In phase 1

---
+++ ProcScribe/ProcScribeOutlook/Task.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ProcScribeOutlook
+{
+    public class Task
+    {
+        public string id { get; set; }
+        public string precedes_id { get; set; }
+        public string parent_workflow_id { get; set; }
+        public string child_workflow_id { get; set; }
+        public string workflow_model_id { get; set; }
+
+        public string user_id { get; set; }
+        public string name { get; set; }
+        public string deadline { get; set; }
+        public string date { get; set; }
+        public string status_id { get; set; }
+        public string type_id { get; set; }
+        public string location_id { get; set; }
+        public string project_id { get; set; }
+        public string follows_id { get; set; }
+
+        //for searching
+         public string process_workflow_id { get; set; }
+
+        //outside Strukt system
+        public string description { get; set; }
+        public string attachmentType { get; set; }
+        public string attachmentDetail { get; set; }
+        public string keyword { get; set; }
+        public string linkDetail { get; set; }
+        
+        //public bool hasAssignmentSent { get; set; }
+        //public bool hasAssignmentReceived { get; set; }
+
+        public static List<Task> getTaskByParentWorkflowID(string strParentWorkflowID)
+        {
+            try
+            {
+                string strReturn = "";
+                StruktMain.StruktMainSoapClient wsStrukt = new StruktMain.StruktMainSoapClient();
+                strReturn = wsStrukt.selectTaskByParentWorkflowID(Strukt.Type_Workflow + strParentWorkflowID);
+                if (strReturn == null)
+                    return null;
+                List<Task> lTask = new List<Task>();
+                lTask = JsonConvert.DeserializeObject<List<Task>>(strReturn);
+                return lTask;
+
+                //string strFlowID = global.getExtractValueFromRespond(
+                //                    global.getRespondFromStruktGet(Strukt.URL_Task, "?parent_workflow_id=" + Strukt.Type_Workflow + strParentWorkflowID));
+
+                //if (strFlowID == "")
+                //    return null;
+
+                //List<Task> taskList = JsonConvert.DeserializeObject<List<Task>>(strFlowID);
+                //return taskList;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static List<Task> getTaskByUserID(string strUserID)
+        {
+            try
+            {
+                string strReturn = "";
+                StruktMain.StruktMainSoapClient wsStrukt = new StruktMain.StruktMainSoapClient();
+                strReturn = wsStrukt.selectTaskByUserID(Strukt.Type_User + strUserID);
+                if (strReturn == null)
+                    return null;
+                List<Task> lTask = new List<Task>();
+                lTask.Add(JsonConvert.DeserializeObject<Task>(strReturn));
+                return lTask;
+
+                //string strUser = global.getExtractValueFromRespond(
+                //                    global.getRespondFromStruktGet(Strukt.URL_Task, "?user_id=" + Strukt.Type_User + strUserID));
+
+                //if (strUser == "")
+                //    return null;
+
+                //List<Task> UserList = JsonConvert.DeserializeObject<List<Task>>(strUser);
+                //return UserList;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static List<Task> getTaskByID(string strID)
+        {
+            try
+            {
+                string strReturn = "";
+                StruktMain.StruktMainSoapClient wsStrukt = new StruktMain.StruktMainSoapClient();
+                strReturn = wsStrukt.selectTask(Strukt.Type_Task + strID);
+                if (strReturn == null)
+                    return null;
+                List<Task> lTask = new List<Task>();
+                lTask.Add(JsonConvert.DeserializeObject<Task>(strReturn));
+                return lTask;
+
+                //string strUser = global.getExtractValueFromRespond(
+                //                    global.getRespondFromStruktGet(Strukt.URL_Task, "/" + strID));
+
+                //if (strUser == "")
+                //    return null;
+
+                //List<Task> UserList = JsonConvert.DeserializeObject<List<Task>>(strUser);
+                //return UserList;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        //Sort the Tasks in List
+        public static List<Task> sortTaskList(List<Task> ltParam)
+        {
+            try
+            {
+                List<Task> ltSort = new List<Task>();
+                Task tFirst = ltParam.Find(o => ((o.follows_id == null) || (o.follows_id == "")));
+                ltSort.Add(tFirst);
+                ltParam.Remove(tFirst);
+                int i = ltParam.Count();
+                string strTaskID = tFirst.id;
+                for (int j = 0; j < i; j++)
+                {
+                    Task tEach = ltParam.Find(o => o.follows_id == strTaskID);
+                    strTaskID = tEach.id;
+                    ltSort.Add(tEach);
+                    ltParam.Remove(tEach);
+                }
+                ltSort.Reverse();
+                return ltSort;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        
+    }
+}
---
+++ ProcScribe/ProcScribeOutlook/TaskProcess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace ProcScribeOutlook 
+{
+
+    public class TaskProcess : Task
+    {
+        public string process_name { get; set; }
+
+        public static List<TaskProcess> getTaskProcessAll()
+        {
+            try
+            {
+                string strReturn = "";
+                ProcScribeOutlook.StruktMain.StruktMainSoapClient wsStrukt = new ProcScribeOutlook.StruktMain.StruktMainSoapClient();
+                strReturn = wsStrukt.selectTaskProcessAll();
+                if (strReturn == null)
+                    return null;
+                List<TaskProcess> lTask = new List<TaskProcess>();
+                lTask = JsonConvert.DeserializeObject<List<TaskProcess>>(strReturn);
+                return lTask;
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
