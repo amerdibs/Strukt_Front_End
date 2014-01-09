dabb0792ec53b9b15ba8320c67f66a2ff70a873d
Pakorn Indhatep <pakorncs@yahoo.com>
2014-01-09 01:16:06 +0100

    add ref into Testing  app.config

---
+++ ProcScribe/ProcScribeTests/WorkflowTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProcScribe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ProcScribe.Tests
+{
+    [TestClass()]
+    public class WorkflowTests
+    {
+        [TestMethod()]
+        public void getWorkflowByIDTest()
+        {
+            List<Workflow> lst = Workflow.getWorkflowByID("31");
+            //List<Workflow> lst = Workflow.getWorkflowByID("");
+            //List<Workflow> lst = Workflow.getWorkflowByID(null);
+            Assert.AreNotEqual(lst,null);
+        }
+
+        [TestMethod()]
+        public void getWorkflowHierarchybyIDTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void addWorkflowTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void editWorkflowTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void deleteWorkflowTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace ProcScribeTests
+{
+    class WorkflowTests
+    {
+    }
+}
